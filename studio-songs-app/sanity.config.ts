89c8b15e087bce06e56b54168870212c412a5bfa
Colin Swinney <colinjswinney@gmail.com>
2025-01-11 23:20:48 +0100

    Add dividers to studio

--- studio-songs-app/sanity.config.ts
+++ studio-songs-app/sanity.config.ts
@@ -3,6 +3,7 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {defaultDocumentNode} from './structure/defaultDocumentNode'
+import {structure} from "./structure"
 
 export default defineConfig({
 	name: 'default',
@@ -11,7 +12,7 @@ export default defineConfig({
 	projectId: 'llxgttbw',
 	dataset: 'production',
 
-	plugins: [structureTool({defaultDocumentNode}), visionTool()],
+	plugins: [structureTool({defaultDocumentNode, structure}), visionTool()],
 
 	schema: {
 		types: schemaTypes,
